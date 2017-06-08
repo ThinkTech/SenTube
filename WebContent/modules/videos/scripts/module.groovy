e9a22a91b2f57f129f714ee8fba86232941a0b3c
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-06-08 17:50:53 +0000

    improving the project.

--- WebContent/modules/videos/scripts/module.groovy
+++ WebContent/modules/videos/scripts/module.groovy
@@ -15,11 +15,14 @@ class VideoAction extends ActionSupport {
 	       def connection = new URL(location).openConnection() as HttpURLConnection
 	       connection.setRequestProperty( 'User-Agent', 'groovy' )
 		   connection.setRequestProperty( 'Accept', 'application/json' )
-	       if ( connection.responseCode == 200 ) { 
+	       if(connection.responseCode == 200) { 
 		      def info = new JsonSlurper().parseText(connection.inputStream.text)
+		      String description = info.items[0].snippet.description
+		      description = description.replace("\"", "").replace("\n", " ")
+		      if(description.length() > 500) description = description.substring(0,500)
+		      info.items[0].snippet.description = description
 		      request.setAttribute("info",info)
 		      SUCCESS
- 
 			} else {
 			  response.sendRedirect(request.contextPath+"/")
 			}
