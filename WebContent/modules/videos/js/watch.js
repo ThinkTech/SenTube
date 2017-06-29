65fe2978fd5486599932fb6bfdc87c453e1822e9
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-06-29 20:02:00 +0000

    improving the project.

--- WebContent/modules/videos/js/watch.js
+++ WebContent/modules/videos/js/watch.js
@@ -1,19 +1,3 @@
-String.prototype.linkify = function() {
-
-    // http://, https://, ftp://
-    var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
-
-    // www. sans http:// or https://
-    var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-
-    // Email addresses
-    var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
-
-    return this.replace(urlPattern, '<a target="_blank" href="$&">$&</a>')
-        .replace(pseudoUrlPattern, '$1<a target="_blank" href="http://$2">$2</a>')
-        .replace(emailAddressPattern, '<a target="_blank" href="mailto:$&">$&</a>');
-};
-
 const display = function(videoId,cache){
 	page.language = localStorage.getItem("language") ? localStorage.getItem("language") : "en";
 	const video = {};
