877e805df2dd181c2fb89791c93b2fef86f369d6
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-06-30 13:12:15 +0000

    improving the project.

--- WebContent/modules/videos/scripts/module.groovy
+++ WebContent/modules/videos/scripts/module.groovy
@@ -13,8 +13,8 @@ class ModuleAction extends ActionSupport {
 	       watch = true
 	       def location = "https://www.googleapis.com/youtube/v3/videos?id=${id}&key=AIzaSyBaYaWQcSP8P1Dau3kxDitRo7W9VA4EOPg&part=snippet"
 	       def connection = new URL(location).openConnection() as HttpURLConnection
-	       connection.setRequestProperty( 'User-Agent', 'groovy' )
-	       connection.setRequestProperty( 'Accept', 'application/json' )
+	       connection.setRequestProperty('User-Agent','groovy')
+	       connection.setRequestProperty('Accept','application/json')
 	       if(connection.responseCode == 200) { 
 		      def info = new JsonSlurper().parseText(connection.inputStream.text)
 		      String description = info.items[0].snippet.description
