3c70d993c83de1b8c2c82fcaf5fbf12ae4bdd1e5
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-07-01 20:33:38 +0000

    improving the project.

--- WebContent/modules/videos/scripts/module.groovy
+++ WebContent/modules/videos/scripts/module.groovy
@@ -17,7 +17,7 @@ class ModuleAction extends ActionSupport {
 	       connection.setRequestProperty('Accept','application/json')
 	       if(connection.responseCode == 200) { 
 		      def info = new JsonSlurper().parseText(connection.inputStream.text)
-		      String description = info.items[0].snippet.description
+		      def description = info.items[0].snippet.description as String
 		      if(description.length() > 500) description = description.substring(0,500)
 		      info.items[0].snippet.description = description.replace("\"", "").replace("\n", " ")
 		      setAttribute("info",info)
